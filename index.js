96ad0ec486be890ec582d5871aebc06442f89159
rishavmandal <rishavmandal40287@gmail.com>
2025-01-31 21:25:41 +0530

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#login').click(function(){
+        $('#loginPage').css("display", "block");
+    });
+    
+});
+$(document).ready(function(){
+    $('#hey').mouseover(function(){
+        $('#hey').animate({height:'400px'})
+    })
+})
